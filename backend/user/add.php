ae98697306acb4d999be1e3ab8d29fc98b2020d4
devbyanuwat <103254881+devbyanuwat@users.noreply.github.com>
2022-04-12 15:18:01 +0700

    Add files via upload

---
+++ backend/user/add.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require('../db.php');
+
+$firstname = $_POST['user_firstname'];
+$lastname = $_POST['user_lastname'];
+$tel = $_POST['user_tel'];
+$username =  $_POST['user_username'];
+$password = $_POST['user_password'];
+
+$sql = "INSERT INTO `member` (`user_id`, `username`, `password`, `name`, `surname`, `tel`, `status_id`) VALUES (NULL, '$username', '$password', '$firstname', '$lastname', '$tel', '2')";
+if (mysqli_query($conn, $sql)) {
+    header('location:../../manage.php');
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
